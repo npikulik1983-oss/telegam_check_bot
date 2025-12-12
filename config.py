4be5b40db215ebba88fe98a8dfdc84ee731c4cef
npikulik1983-oss <npikulik1983@gmail.com>
2025-12-12 15:16:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+CHANNEL_ID_STR = os.getenv("CHANNEL_ID")
+
+if not BOT_TOKEN:
+    raise RuntimeError("Не задана переменная окружения BOT_TOKEN")
+
+if not CHANNEL_ID_STR:
+    raise RuntimeError("Не задана переменная окружения CHANNEL_ID")
+
+CHANNEL_ID = int(CHANNEL_ID_STR)
 
